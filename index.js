29dc22d5f4d7c270f5b2c4f2415bcb91eb87a537
tkyg <80114433+tkyg@users.noreply.github.com>
2022-04-26 13:25:40 -0400

    commit tree 31d974e08179ea58c356fad7b56db351ce89f6c1

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+// function saturdayFun(activity = "roller-skate"){
+//     switch (activity){
+//         case "bathe my dog":
+//             return "This Saturday, I want to bathe my dog!"
+//                 default:
+//                     return `This Saturday, I want to ${activity}!`
+//     }
+// }
+
+// function mondayWork( activity = 'go to the office'){
+//     switch (activity){
+//         case "work from home":
+//             return "This Monday, I will work from home."
+//             default:
+//                 return `This Monday, I will ${activity}.`
+//     }
+// }
+
+ 
+// function wrapAdjective(title1 = "*", title2 = "You are"){
+//     return function (title3){
+//       return `${title2} ${title1}${title3}${title1}!`;
+//     }
+// }
+
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("swim")
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("go play golf")
+
+function wrapAdjective(symbol = "*", title = "You are"){
+    return function (adjective = "special"){
+        return `${title} ${symbol}${adjective}${symbol}!`
+    }
+}
